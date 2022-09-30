ef16de4afcc3aed591f6e2147ac3a34c42012e15
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-30 19:40:49 +0300

    Link to confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { confirmations: 'users/confirmations', omniauth_callbacks: 'users/omniauth_callbacks' }
   root "home#index"
-  get 'users', to: 'users#index'
+ resources :users
 
 end
