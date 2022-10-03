3238ab7a69cc109954bc3bfd4d6e0d407fcb5ae8
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-03 11:21:34 +0300

    Config main url in thr prod mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.routes.default_url_options[:host] = "https://radiant-headland-14852.herokuapp.com/"
